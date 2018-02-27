f8e7462a6d3ef34cc69a30dd1b577e376f9b0750
Jitendra <jitendra.thethi@aricent.com>
2018-02-27 18:38:40 +0530

    static files route

--- qcapp/app.js
+++ qcapp/app.js
@@ -23,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//all public files exposed on /
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
